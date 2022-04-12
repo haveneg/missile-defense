c6909a95f28bf5a47cbadb72dccbc2d57be626b6
haveneg <havenedward24@gmail.com>
2022-04-12 17:01:37 -0500

    game window set up

--- game.js
+++ game.js
@@ -2,8 +2,22 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 function resize() {
-    canvas.width = window.innerWidth - 40;
-    canvas.height = window.innerHeight - 40;
+    canvas.width = window.innerWidth - 30;
+    canvas.height = window.innerHeight - 30;
 }
 
+resize();
 window.addEventListener('resize', resize);
+
+function step() {
+    console.log("step");
+
+    ctx.fillStyle = "rgb(0, 255, 0)";
+    ctx.fillRect(50, 50, 20, 20);
+
+    requestAnimationFrame(step);
+}
+
+window.addEventListener('load', () => {
+    step();
+});
